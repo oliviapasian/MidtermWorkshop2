167917482527ebad522cff50214d1e8157bf62cd
oliviapasian <99832146+oliviapasian@users.noreply.github.com>
2024-02-14 22:56:14 -0500

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,4 @@
+Resources used:
+1. Pop-up window:
+    https://www.w3schools.com/howto/howto_css_modals.asp
+    https://codepen.io/williamliaw/pen/gPMdrm
---
+++ script.js
@@ -0,0 +1,68 @@
+$('#md-trigger').on('click', function(e) {
+    $('#modal-1').toggleClass("md-show");
+    e.preventDefault();
+  });
+
+$('#md-close').on('click', function(e) {
+    $('#modal-1').toggleClass("md-show");
+    e.preventDefault();
+  });
+
+
+
+  $('#md-trigger2').on('click', function(e) {
+    $('#modal-2').toggleClass("md-show");  
+    e.preventDefault();
+  });
+
+$('#md-close2').on('click', function(e) {
+    $('#modal-2').toggleClass("md-show"); 
+    e.preventDefault();
+  });
+
+
+
+  $('#md-trigger3').on('click', function(e) {
+    $('#modal-3').toggleClass("md-show");  
+    e.preventDefault();
+  });
+
+$('#md-close3').on('click', function(e) {
+    $('#modal-3').toggleClass("md-show"); 
+    e.preventDefault();
+  });
+
+
+
+  $('#md-trigger4').on('click', function(e) {
+    $('#modal-4').toggleClass("md-show");  
+    e.preventDefault();
+  });
+
+$('#md-close4').on('click', function(e) {
+    $('#modal-4').toggleClass("md-show"); 
+    e.preventDefault();
+  });
+
+
+  $('#md-trigger5').on('click', function(e) {
+    $('#modal-5').toggleClass("md-show");  
+    e.preventDefault();
+  });
+
+$('#md-close5').on('click', function(e) {
+    $('#modal-5').toggleClass("md-show"); 
+    e.preventDefault();
+  });
+
+
+  $('#md-trigger6').on('click', function(e) {
+    $('#modal-6').toggleClass("md-show");  
+    e.preventDefault();
+  });
+
+$('#md-close6').on('click', function(e) {
+    $('#modal-6').toggleClass("md-show"); 
+    e.preventDefault();
+  });
+  
